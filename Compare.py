bf84b17bd5b5e102f8e445f8e8c6e23dc6752623
berryee <shell203566@gmail.com>
2022-07-15 03:26:27 +0800

    First

---
+++ Compare.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df1=pd.read_excel('LJ-D1.xlsx')
+df2=pd.read_excel('compare.xlsx')
+
+df1.equals(df2)
+
+comparison_values = df1.values == df2.values
+print (comparison_values)
+
+import numpy as np
+rows,cols=np.where(comparison_values==False)
+
+for item in zip(rows,cols):
+    df1.iloc[item[0], item[1]] = '{} --> {}'.format(df1.iloc[item[0], item[1]],df2.iloc[item[0], item[1]])
+
+df1.to_excel('./Excel_diff_D1wNR.xlsx',index=False,header=True)
